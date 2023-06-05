c3bcc2d56d35f7dbb2a6c4dbf5136ae7d5324b6a
Nicolas Hoffmann <nicolas.hoffmann@stud.htwk-leipzig.de>
2023-06-05 23:05:57 +0200

    Added Reservation

--- src/app/Pages/film/filmoverview.page.ts
+++ src/app/Pages/film/filmoverview.page.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
+import { firstValueFrom } from 'rxjs';
+import { ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +11,57 @@ import { Component } from '@angular/core';
   styleUrls: ['filmoverview.page.scss']
 })
 export class FilmOverviewPage {
+  @ViewChild(IonModal) modal!: IonModal;
 
-  constructor() {}
+  selectedSeats: any[] = [];
+  totalPrice: number = 0;
+  message: string = '';
 
+  isOpen: boolean = false;
+  constructor(private toastController: ToastController) { }
+
+  openTimes() {
+    this.isOpen = !this.isOpen;
+  }
+
+  openModal() {
+    const modal = document.querySelector('ion-modal');
+    modal!.present();
+  }
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.selectedSeats, 'confirm');
+  }
+
+  toggleSeatSelection(seatIndex: number) {
+    if (this.selectedSeats.includes(seatIndex)) {
+      this.selectedSeats = this.selectedSeats.filter(seat => seat !== seatIndex);
+    } else {
+      this.selectedSeats.push(seatIndex);
+    }
+
+  }
+  async onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<any[]>>;
+    if (ev.detail.role === 'confirm') {
+      const seats = ev.detail.data;
+      const seatPrice = 9;
+      const totalPrice = seats!.length * seatPrice;
+
+      const message = `Anzahl Plätze: ${seats!.length}\nPreis: ${totalPrice}€`;
+      await this.showToast(message);
+    }
+  }
+
+  async showToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 3000,
+      position: 'bottom'
+    });
+    await toast.present();
+  }
 }
