9c9c82dd9cd3af8c74a9e53145c42a6c47075d6b
Nicolas Hoffmann <nicolas.hoffmann@stud.htwk-leipzig.de>
2023-06-21 11:08:32 +0200

    Implement Suche

--- src/app/Pages/film/filmoverview.page.ts
+++ src/app/Pages/film/filmoverview.page.ts
@@ -4,13 +4,22 @@ import { OverlayEventDetail } from '@ionic/core/components';
 import { Subject, Subscription, debounceTime, firstValueFrom } from 'rxjs';
 import { ToastController } from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
-
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'filmoverview.page.html',
   styleUrls: ['filmoverview.page.scss'],
+  animations: [
+    trigger('openClose', [
+      state('true', style({ opacity: 0, 'font-size': '0', height: '0' })),
+      state('false', style({ opacity: 1, 'font-size': '*', height: '*' })),
+      transition('false <=> true', [
+        animate('400ms ease-in-out')
+      ])
+    ])
+  ]
 })
 export class FilmOverviewPage implements OnInit, OnDestroy {
   @ViewChild(IonModal) modal!: IonModal;
@@ -21,6 +30,7 @@ export class FilmOverviewPage implements OnInit, OnDestroy {
   totalPrice: number = 0;
   message: string = '';
   isOpen: boolean[] = [];
+  isSearchOpen: boolean = false;
   isModalOpen: boolean = false;
   detailView: boolean = true;
   showFull: boolean[] = [];
@@ -29,10 +39,25 @@ export class FilmOverviewPage implements OnInit, OnDestroy {
   filteredFilms: any[] = [];
   private searchSubject: Subject<string> = new Subject<string>();
   sub: Subscription = new Subscription;
+  selectedFilters: any = {
+    genresTags: [],
+    tageAuswahl: [],
+    leinwandHighlights: [],
+    extras: [],
+    flags: [],
+    behindertenTags: []
+  };
 
-  filters = ['Genre', 'Kinosaal', 'Sound', 'Barrierefreie Optionen']
+  filters = ['Zeitraum', 'Genre', 'Kinosaal', 'Sound', 'Barrierefreie Optionen']
 
-  tageAuswahl = ['tage_auswahl-heute', 'tage_auswahl-morgen', 'tage_auswahl-amwe', 'tage_auswahl-naechstewoche', 'tage_auswahl-weiterenwochen'];
+  tageAuswahl = [
+    { id: '', name: 'Diese Woche' },
+    { id: 'tage_auswahl-heute', name: 'Heute' },
+    { id: 'tage_auswahl-morgen', name: 'Morgen' },
+    { id: 'tage_auswahl-amwe', name: 'Am Wochenende' },
+    { id: 'tage_auswahl-naechstewoche', name: 'nächste Woche' },
+    { id: 'tage_auswahl-weiterenwochen', name: 'alle weiteren Wochen' },
+  ];
 
   genresTag = [
     { id: 165060, name: 'Action' },
@@ -61,13 +86,16 @@ export class FilmOverviewPage implements OnInit, OnDestroy {
     { id: 548180, name: 'Manhatten' },
     { id: 1053804, name: 'Onyx LED' }
   ];
-
+  customActionSheetOptions = {
+    header: 'Colors',
+    subHeader: 'Select your favorite color',
+  };
   extras = ['neustarts', 'vorverkauf'];
 
   flags = [
     { id: 104836, name: 'ATMOS' },
     { id: 104831, name: '3D' },
-    { id: 104831, name: 'OV' },
+    { id: 104837, name: 'OV' },
     { id: 104838, name: 'mit Untertitel' },
     { id: 631455, name: 'D-BOX' }
   ];
@@ -93,7 +121,11 @@ export class FilmOverviewPage implements OnInit, OnDestroy {
     }
   }
 
-  showTags(index: number){
+  openSearch() {
+    this.isSearchOpen = !this.isSearchOpen;
+  }
+
+  showTags(index: number) {
     this.showAllTags[index] = !this.showAllTags[index];
   }
 
@@ -125,20 +157,6 @@ export class FilmOverviewPage implements OnInit, OnDestroy {
     return leinwand.release_flags.some((flag: any) => flag.flag_name === name);
   }
 
-
-  // scrollToGrid(index: number) {
-  //   const gridElement = document.getElementById(`gridRef-${index}`);
-  //   if (gridElement) {
-  //     const gridOffsetTop = gridElement.offsetTop;
-  //     const gridHeight = gridElement.offsetHeight;
-  //     const windowHeight = window.innerHeight;
-  //     const scrollPosition = gridOffsetTop - (windowHeight - gridHeight) / 2;
-
-  //     this.content.scrollToPoint(0, scrollPosition, 500); // Adjust the duration (ms) as needed
-  //   }
-  // }
-
-
   async scrollToGrid(index: number) {
     const gridElement: HTMLElement | null = document.querySelector(`#gridRef-${index}`);
     if (gridElement) {
@@ -166,6 +184,10 @@ export class FilmOverviewPage implements OnInit, OnDestroy {
     this.sub = this.searchSubject.pipe(debounceTime(500)).subscribe(() => {
       this.updateFilteredFilms();
     });
+    // Set default selected value for tageAuswahl
+    this.selectedFilters.tageAuswahl = this.tageAuswahl[0].id;
+    // Set default selected value for leinwandHighlights
+    this.selectedFilters.leinwandHighlights = this.leinwandHighlights[0].id;
   }
 
   ngOnDestroy() {
@@ -174,27 +196,54 @@ export class FilmOverviewPage implements OnInit, OnDestroy {
 
   async loadFilmData() {
     // Check if the films data is already stored
-    const storedFilmsData = localStorage.getItem('filmsData');
-    if (storedFilmsData) {
-      // Use the stored data instead of making a request
-      this.films = JSON.parse(storedFilmsData);
-      await this.updateFilteredFilms();
-      console.log(this.films);
-      return; // Exit the function since data is already available
-    }
+    // const storedFilmsData = localStorage.getItem('filmsData');
+    // if (storedFilmsData) {
+    //   // Use the stored data instead of making a request
+    //   this.films = JSON.parse(storedFilmsData);
+    //   await this.updateFilteredFilms();
+    //   console.log(this.films);
+    //   return; // Exit the function since data is already available
+    // }
 
-    // Films data is not stored, make the request
-    const url =
-      'http://localhost:8080/https://www.cinecitta.de/common/ajax.php?bereich=portal&modul_id=101&klasse=vorstellungen&cli_mode=1&com=anzeigen_spielplan';
+    const url = 'http://localhost:8080/https://www.cinecitta.de/common/ajax.php';
+    const params = {
+      bereich: 'portal',
+      modul_id: '101',
+      klasse: 'vorstellungen',
+      cli_mode: '1',
+      com: 'anzeigen_spielplan',
+    };
+
+    const formData = new FormData();
+    formData.append('get_filter_aktiv', 'false');
+    formData.append('filter[ovfilme]', '0');
+
+    // Append selected filters to the form data
+    this.selectedFilters.genresTags.forEach((id: number) => formData.append('filter[genres_tags][]', id.toString()));
+    if (this.selectedFilters.leinwandHighlights.length > 0) {
+      formData.append('filter[leinwand_highlight]', this.selectedFilters.leinwandHighlights[0].toString());
+    }
+    if (this.selectedFilters.tageAuswahl.length > 0) {
+      formData.append('filter[tage_auswahl]', this.selectedFilters.tageAuswahl[0]);
+    }
+    this.selectedFilters.extras.forEach((extra: string) => formData.append('filter[extra][]', extra));
+    this.selectedFilters.flags.forEach((id: number) => formData.append('filter[releasetypen_flags][]', id.toString()));
+    this.selectedFilters.behindertenTags.forEach((id: number) => formData.append('filter[barrierefrei_tags][]', id.toString()));
+
+    // formData.append('filter[rangeslide][]', 'Zeit eins');
+    // formData.append('filter[rangeslide][]', 'Zeit zwei');
+
     try {
-      const response: any = await firstValueFrom(this.http.post(url, null));
+      const response: any = await firstValueFrom(this.http.post(url, formData, { params }));
+      console.log(response); // Log the response to inspect its structure
       const filmsData = response?.daten?.items ?? [];
       this.films = filmsData;
+      this.filteredFilms = this.films;
       await this.updateFilteredFilms();
-      console.log(this.films);
-
-      // Store the films data in local storage for future use
+      // console.log(this.filteredFilms);
       localStorage.setItem('filmsData', JSON.stringify(filmsData));
+
+
     } catch (error) {
       console.error(error);
     }
@@ -203,8 +252,6 @@ export class FilmOverviewPage implements OnInit, OnDestroy {
   onSearchChange(searchValue: string) {
     this.searchQuery = searchValue.trim().toLowerCase();
     this.searchSubject.next(this.searchQuery);
-    // If you want to immediately update the filtered films without debounce, uncomment the following line
-    // this.updateFilteredFilms();
   }
 
   async updateFilteredFilms() {
@@ -230,123 +277,67 @@ export class FilmOverviewPage implements OnInit, OnDestroy {
     modal!.present();
   }
 
-  cancel() {
-    this.modal.dismiss(null, 'cancel');
-    this.setOpen(false);
+  async toggleSelection(id: any, filterType: string) {
+    if (filterType === 'leinwandHighlights' || filterType === 'tageAuswahl') {
+      // For Kinosaal tag
+      this.selectedFilters[filterType] = [id];
+    } else {
+      // For other tags
+      const index = this.selectedFilters[filterType].indexOf(id);
+      if (index > -1) {
+        // Remove the id if already selected
+        this.selectedFilters[filterType].splice(index, 1);
+      } else {
+        // Add the id if not selected
+        this.selectedFilters[filterType].push(id);
+      }
+    }
+    await this.loadFilmData();
   }
 
-  confirm() {
-    this.modal.dismiss(this.selectedSeats, 'confirm');
-    this.setOpen(false);
-  }
-
-  toggleSeatSelection(seatIndex: number) {
-    if (this.selectedSeats.includes(seatIndex)) {
-      this.selectedSeats = this.selectedSeats.filter(
-        (seat) => seat !== seatIndex
+  isSelected(id: any, filterType: string): boolean {
+    if (filterType === 'leinwandHighlights') {
+      // For Kinosaal tag
+      return (
+        this.selectedFilters[filterType].includes(id) ||
+        (this.selectedFilters[filterType].length === 0 && id === 171984)
+      );
+    } else if (filterType === 'tageAuswahl') {
+      // For tageAuswahl tag
+      return (
+        this.selectedFilters[filterType].includes(id) ||
+        (this.selectedFilters[filterType].length === 0 && id === '')
       );
     } else {
-      this.selectedSeats.push(seatIndex);
-    }
-  }
-  async onWillDismiss(event: Event) {
-    const ev = event as CustomEvent<OverlayEventDetail<any[]>>;
-    if (ev.detail.role === 'confirm') {
-      const seats = ev.detail.data;
-      const seatPrice = 9;
-      const totalPrice = seats!.length * seatPrice;
-
-      const message = `Anzahl Plätze: ${seats!.length}\nPreis: ${totalPrice}€`;
-      await this.showToast(message);
+      // For other tags
+      return this.selectedFilters[filterType].includes(id);
     }
   }
 
-  async showToast(message: string) {
-    const toast = await this.toastController.create({
-      message: message,
-      duration: 3000,
-      position: 'bottom',
-    });
-    await toast.present();
+  async cancel() {
+    this.showAllTags = Array(this.filters.length).fill(false);
+    this.showAllTags = this.showAllTags.map(_ => false);
+    this.setOpen(false);
+  }
+
+  async confirm() {
+    await this.loadFilmData();
+    this.showAllTags = this.showAllTags.map(_ => false);
+    this.setOpen(false);
+  }
+
+  reset() {
+    // Store the current selection of the tag with ID 171984
+    const isSelected171984 = this.selectedFilters.leinwandHighlights.includes(171984);
+
+    // Reset selected filters
+    this.selectedFilters.genresTags = [];
+    this.selectedFilters.leinwandHighlights = isSelected171984 ? [171984] : [];
+    this.selectedFilters.extras = [];
+    this.selectedFilters.flags = [];
+    this.selectedFilters.behindertenTags = [];
+
+    // Reset background color of tags
+    this.showAllTags = this.showAllTags.map(_ => false);
   }
 }
-
-
-/************************************************************************************************************************************************************/
-/************************************************************************************************************************************************************/
-/***************  The following Part contains Logic used for filtering films in realtime on the server side, this will be used in production  ***************/
-/************************************************************************************************************************************************************/
-/************************************************************************************************************************************************************/
-/************************************************************************************************************************************************************/
-
-
-
-// async ngOnInit(
-//   this.sub = this.searchSubject
-//     .pipe(debounceTime(700))
-//     .subscribe(() => {
-//       this.loadFilmData();
-//     });
-//  )
-
-
-// const tage_auswahl = empty, tage_auswahl-heute, tage_auswahl-morgen, tage_auswahl-amwe, tage_auswahl-naechstewoche, tage_auswahl-weiterenwochen
-// const genres_tag = 165060(Action), 165087(Animation), 165579(Dokumentation), 165122(Drama), 165088(Family), 165058(Fantasy), 165110(Horror), 165033(Komödie), 165137(Romance), 165059(Science Fiction), 165156(Thriller), 262715(Anime), 843164(Arthouse),
-// const leinwand_highlights = 171984(Alle Kinos), 168943(CINECITTA), 1039(Deluxe), 1040(Cinemagnum), 185228(Open Air), 121383(Meisengeige), 122646(Metropolis), 548180(Manhatten), 1053804(Onyx LED)
-// const extras = neustarts, vorverkauf
-// const flags = 104836(ATMOS), 104831(3D), 104831(OV), 104838(mit Untertitel), 631455(D-BOX)
-// const behinderten_tags = 1(Hörverstärkung), 2(Audiodeskription), 3(Untertitel für Hörgeschädigte)
-
-//  async loadFilmData() {
-//     Check if the films data is already stored
-//   const storedFilmsData = localStorage.getItem('filmsData');
-//   if (storedFilmsData) {
-//     // Use the stored data instead of making a request
-//     this.films = JSON.parse(storedFilmsData);
-//     await this.updateFilteredFilms();
-//     console.log(this.films);
-//     return; // Exit the function since data is already available
-//   }
-
-//   // Films data is not stored, make the request
-//   const url =
-//     'http://localhost:8080/https://www.cinecitta.de/common/ajax.php';
-//   const params = {
-//     bereich: 'portal',
-//     modul_id: '101',
-//     klasse: 'vorstellungen',
-//     cli_mode: '1',
-//     com: 'anzeigen_spielplan',
-//   };
-//   const formData = new FormData()
-//   formData.append('get_filter_aktiv', 'false');
-//   formData.append('filter[ovfilme]', '0')
-//   formData.append('filter[stichwort]', this.searchQuery);
-//   formData.append('filter[barrierefrei_tags][]', 'behinderten_tags')
-//   formData.append('filter[genres_tags][]', 'genres_tag')
-//   formData.append('filter[extra][]', 'extras')
-//   formData.append('filter[leinwand_highlight]', 'leinwand_highlights')
-//   formData.append('filter[rangeslide][]', 'Zeit eins')
-//   formData.append('filter[rangeslide][]', 'Zeit zwei')
-//   formData.append('filter[tage_auswahl]', 'tage_auswahl')
-//   formData.append('filter[releasetypen_flags][]', 'flags')
-
-//   try {
-//     const response: any = await firstValueFrom(this.http.post(url, formData, { params }));
-//     console.log(response); // Log the response to inspect its structure
-//     const filmsData = response?.daten?.items ?? [];
-//     this.films = filmsData;
-//     await this.updateFilteredFilms();
-//     console.log(this.films);
-
-//     // Store the films data in local storage for future use
-//     localStorage.setItem('filmsData', JSON.stringify(filmsData));
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// onSearchChange(searchValue: string) {
-//   this.searchQuery = searchValue.trim().toLowerCase();
-//   this.searchSubject.next(this.searchQuery);
-// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
