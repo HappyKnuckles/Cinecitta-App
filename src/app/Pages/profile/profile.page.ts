bd43cef6dc0523a36340066e94a615f5dbcacf75
nicolas.hoffmann <nicolas.hoffmann@stud.htwk-leipzig.de>
2023-10-30 16:24:08 +0100

    Template für Profil vorbereitet

--- src/app/Pages/profile/profile.page.ts
+++ src/app/Pages/profile/profile.page.ts
@@ -12,9 +12,15 @@ import { Subscription, firstValueFrom } from 'rxjs';
 export class ProfilePage implements OnInit {
   @ViewChild('modal1') modal1!: IonModal;
   @ViewChild('modal2') modal2!: IonModal;
+  @ViewChild('modal0') modal0!: IonModal;
   username: string = '';
   password: string = '';
 
+  manhattanFilms: any[] = [];
+  cinecittaFilms: any[] = [];
+  futureFilms: any[] = [];
+  filmsInCurrentMonth: any[] = [];
+  filmsInCurrentYear: any[] = [];
   history: any[] = [];
   currentTags = undefined;
   isOpen: boolean[] = [];
@@ -42,24 +48,23 @@ export class ProfilePage implements OnInit {
     },
   ];
 
-  image = "assets/images/default-avatar.png";
-  image2 = "assets/images/hellow.png";
-  sub: Subscription = new Subscription;
+  image = 'assets/images/default-avatar.png';
+  image2 = 'assets/images/hellow.png';
+  sub: Subscription = new Subscription();
   isToggled: boolean = false;
-  constructor(private router: Router,
-    private http: HttpClient
-  ) {
-  }
+  constructor(private router: Router, private http: HttpClient) {}
 
   async ngOnInit() {
     await this.loadTicketHistory();
-  }
-
+    await this.getFilmsForCurrentMonthAndYear();
+    await this.getFutureFilms();
+    await this.getCinecittaAndManhattanFilms();
+  } 
 
   async login(username: string, password: string): Promise<boolean> {
-    const url = "https://proxy-server-rho-pearl.vercel.app/api/server";
+    const url = 'https://proxy-server-rho-pearl.vercel.app/api/server';
     const formData = new URLSearchParams();
-  
+
     const params = {
       bereich: 'portal',
       modul_id: '1',
@@ -71,7 +76,7 @@ export class ProfilePage implements OnInit {
     });
     formData.append('login_kennung', username); // Use 'login_kennung' as the field name for username
     formData.append('login_passwort', password); // Use 'login_passwort' as the field name for password
-  
+
     try {
       const response: any = await firstValueFrom(
         this.http.post(url, formData.toString(), {
@@ -80,45 +85,72 @@ export class ProfilePage implements OnInit {
           observe: 'response', // Include the response headers
         })
       );
-  
-    
-        // Extract cookies from the response's Set-Cookie header
-        const setCookieHeader = response.headers.get('set-cookie');
-        console.log(setCookieHeader);
-        // Save cookies in local storage
-        localStorage.setItem('appCookies', setCookieHeader);
-  
-        // Perform any additional actions upon successful login
-        console.log('Login successful');
-        return true;
+
+      // Extract cookies from the response's Set-Cookie header
+      const setCookieHeader = response.headers.get('set-cookie');
+      console.log(setCookieHeader);
+      // Save cookies in local storage
+      localStorage.setItem('appCookies', setCookieHeader);
+
+      // Perform any additional actions upon successful login
+      console.log('Login successful');
+      return true;
     } catch (error) {
       console.error(error);
       return false;
     }
   }
-  
 
+  async getCinecittaAndManhattanFilms(){
+    this.manhattanFilms = this.history.filter((ticket) => {
+      return ticket.buchung_vorstellung_theater_id !== 382;
+    })
+
+    this.cinecittaFilms = this.history.filter((ticket) => {
+      return ticket.buchung_vorstellung_theater_id === 382;
+    })
+  }
+
+  async getFutureFilms() {
+    this.futureFilms = this.history.filter((ticket) => {
+      return ticket.transaktion_zeitlich !== 'vergangen';
+    });
+  }
+  
+  async getFilmsForCurrentMonthAndYear() {
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth() + 1; // Months are zero-based
+    const currentYear = currentDate.getFullYear();
+
+    this.filmsInCurrentMonth = this.history.filter((ticket) => {
+      const ticketDate = new Date(ticket.buchung_vorstellung_start_datum);
+      return (
+        ticketDate.getMonth() + 1 === currentMonth && ticketDate.getFullYear() === currentYear
+      );
+    });
+
+    this.filmsInCurrentYear = this.history.filter((ticket) => {
+      const ticketDate = new Date(ticket.buchung_vorstellung_start_datum);
+      return ticketDate.getFullYear() === currentYear;
+    });
+  }
 
   async loadTicketHistory() {
- // Retrieve cookies from local storage
- const savedCookies = localStorage.getItem('appCookies');
+    // Retrieve cookies from local storage
 
- // Check if cookies were saved
- if (savedCookies) {
-   // Include cookies in the headers
-   console.log("True");
-   const headers = new HttpHeaders({
-     'Content-Type': 'application/x-www-form-urlencoded',
-     'Cookie': savedCookies, // Set the appropriate headers for cookies
-   });
+    // Check if cookies were saved
+    // Include cookies in the headers
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+    });
     // Ticket data is not stored, make the request
-    const url = "https://proxy-server-rho-pearl.vercel.app/api/server";
+    const url = 'https://proxy-server-rho-pearl.vercel.app/api/server';
     const formData = new URLSearchParams();
-    formData.append('filter[filters][0][value]', "4")
-    formData.append('filter[filters][0][field]', "bereich")
-    formData.append('filter[filters][0][operator]', "eq")
-    formData.append('filter[logic]', "and")
-    formData.append('take', "5")
+    formData.append('filter[filters][0][value]', '4');
+    formData.append('filter[filters][0][field]', 'bereich');
+    formData.append('filter[filters][0][operator]', 'eq');
+    formData.append('filter[logic]', 'and');
+    formData.append('take', '5');
 
     const params = {
       bereich: 'portal',
@@ -128,16 +160,13 @@ export class ProfilePage implements OnInit {
       cli_mode: '1',
     };
 
-    
-
     try {
       const response: any = await firstValueFrom(
         this.http.post(url, formData.toString(), {
           params: params,
           headers: headers,
         })
-      ); 
-       console.log(response); // Log the response to inspect its structure
+      );
       const ticketData = response?.daten?.items ?? [];
       this.history = ticketData;
       console.log(this.history);
@@ -147,7 +176,7 @@ export class ProfilePage implements OnInit {
     } catch (error) {
       console.error(error);
     }
-  }}
+  }
 
   openDetails(index: number) {
     // Toggle the open state for the clicked item
@@ -170,6 +199,7 @@ export class ProfilePage implements OnInit {
   cancel() {
     this.modal1.dismiss(null, 'cancel');
     this.modal2.dismiss(null, 'cancel');
+    this.modal0.dismiss(null, 'cancel');
   }
 
   confirm() {
