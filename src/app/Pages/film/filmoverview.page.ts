bc8731dd26535d9a68a783380043d763788b88a4
Nicolas Hoffmann <82639648+HappyKnuckles@users.noreply.github.com>
2024-08-12 19:19:27 +0200

    Update filmoverview.page.ts

--- src/app/Pages/film/filmoverview.page.ts
+++ src/app/Pages/film/filmoverview.page.ts
@@ -82,7 +82,8 @@ export class FilmOverviewPage implements OnInit, OnDestroy {
         })
     );
     this.setDefaultSelectedFilterValues();
-    await this.onTimeChange();
+    //await this.onTimeChange();
+    await this.loadFilmData();
     this.startPeriodicCheck();
   }
   startPeriodicCheck() {
