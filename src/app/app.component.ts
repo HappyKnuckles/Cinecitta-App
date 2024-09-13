7081621edd8a7daf1294fca5155647ac988eb0b4
Nicolas Hoffmann <hoffmannni92690@th-nuernberg.de>
2024-09-14 00:07:52 +0200

    Changed change format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,7 +43,7 @@ export class AppComponent implements OnDestroy {
 
             if (newCommits.length > 0) {
               const commitMessages = newCommits.join('\n');
-              if (confirm(`A new version is available. Changes:\n${commitMessages}\nLoad it?`)) {
+              if (confirm(`A new version is available. Load it?\nChanges:\n${commitMessages}\n`)) {
                 localStorage.setItem('lastCommitSha', data[0].sha);
                 window.location.reload();
               }
